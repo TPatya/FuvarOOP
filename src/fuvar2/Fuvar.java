b7cd3ea05c99a558a460259592fcb757157cf151
szevasz <163414309+TPatya@users.noreply.github.com>
2025-04-07 12:50:59 +0200

    Olvasás

--- src/fuvar2/Fuvar.java
+++ src/fuvar2/Fuvar.java
@@ -4,10 +4,55 @@
  */
 package fuvar2;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author patri
  */
 public class Fuvar {
+    private int azonosito;
+    public int getAzonosito(){
+        return this.azonosito;
+    }
+    private LocalDateTime indulasido;
+    public LocalDateTime getIndulasIdo(){
+        return this.indulasido;
+    }
+    private int utazasido;
+    public int getUtazasIdo(){
+        return this.utazasido;
+    }
+    private double megtettTav;
+    public double getMegtettTav(){
+        return this.megtettTav;
+    }
+    private double viteldij;
+    public double getViteldij(){
+        return this.viteldij;
+    }
+    private double borravalo;
+    public double getBorraValo(){
+        return this.borravalo;
+    }
+    private String fizetesmod;
+    public String getFizetesMod(){
+        return this.fizetesmod;
+    }
     
+    
+    //konstruktor
+    
+    public Fuvar(String sor){
+        String[] db = sor.split(";");
+        this.azonosito = Integer.parseInt(db[0]);
+        this.indulasido = LocalDateTime.parse(db[1],DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.utazasido = Integer.parseInt(db[2]);
+        this.megtettTav = Double.parseDouble(db[3].replace(",", "."));
+        this.viteldij = Double.parseDouble(db[4].replace(",", "."));
+        this.borravalo = Double.parseDouble(db[5].replace(",", "."));
+        this.fizetesmod = db[6];
+    }
 }
